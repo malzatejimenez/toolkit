2d8c7b4014d5923289059f79d8db439202bc7d27
malzatejimenez <alzate.jimenez.09@gmail.com>
2022-12-01 08:42:25 -0500

    Add slugify method and tests

--- tools.go
+++ tools.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -49,7 +50,6 @@ func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool)
 
 	return files[0], nil
 }
-
 
 // UploadFiles uploads one or more file to a specified directory, and gives files as random name.
 // It returns a slice containing the newly named files, the original file names, the size of the files,
@@ -151,12 +151,11 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 	return uploadedFiles, nil
 }
 
-
 // CreateDirIfNotExist creates a directory, and all necessary parents, if it does not exist
-func (t *Tools)CreateDirIfNotExist(path string)error{
+func (t *Tools) CreateDirIfNotExist(path string) error {
 	const mode = 0755
 
-	if _, err := os.Stat(path); os.IsNotExist(err){
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err := os.MkdirAll(path, mode)
 		if err != nil {
 			return err
@@ -164,4 +163,20 @@ func (t *Tools)CreateDirIfNotExist(path string)error{
 	}
 
 	return nil
+}
+
+// Slugify is a (very) simple means of creating a slug from a string
+func (t *Tools) Slugify(s string) (string, error) {
+	if s == "" {
+		return "", errors.New("empty string not permitted")
+	}
+
+	var re = regexp.MustCompile(`[^a-z\d]+`)
+
+	slug := strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
+	if len(slug) == 0 {
+		return "", errors.New("after removing characters, slug is zero length")
+	}
+
+	return slug, nil
 }
